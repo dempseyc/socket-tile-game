b7341cb41ea0be9a0c6b58ca5983b8d249187a75
Craig Dempsey <craigdempsey@gmail.com>
2017-10-13 22:55:09 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const path = require('path');
 const mustacheExpress = require('mustache-express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
